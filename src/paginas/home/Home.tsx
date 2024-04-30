3eadd2a9f45a39d40ce1ecf455d314e908388742
Pedro Henrique <pedrouser9@gmail.com>
2024-04-30 17:07:40 -0300

    muita coisa

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-
+import homeLogo from '../../assets/home.png'
 import './Home.css';
-
 
 function Home() {
     return (
@@ -18,10 +17,13 @@ function Home() {
               </div>
             </div>
   
-
+            <div className="flex justify-center ">
+              <img src={homeLogo} alt="" className='w-2/3' />
+      
+            </div>
           </div>
         </div>
-      
+        
       </>
     );
 }
